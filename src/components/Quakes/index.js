b0a9df76231b05e4ba81fff938d964b3aaa1ba77
Joy <stepjoyous@gmail.com>
2019-01-25 09:32:31 -0800

    Created new components: Quakes and Map.

---
+++ src/components/Quakes/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Quakes extends from Component {
+
+    render () {
+        return (
+            <h1>Earthquakes from the Past Week </h1>
+        )
+    }
+
+}
